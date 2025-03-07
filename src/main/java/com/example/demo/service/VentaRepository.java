f55141e1460d06a5bf730f99d78a18b1cfa17004
Bladimir Landa <bladimircarrillolanda02@gmail.com>
2025-03-07 14:28:46 -0600

    Update: Se actualizan cosas no necesarias

--- src/main/java/com/example/demo/service/VentaRepository.java
+++ src/main/java/com/example/demo/service/VentaRepository.java
@@ -2,8 +2,5 @@ package com.example.demo.service;
 
 import com.example.demo.dto.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
+public interface VentaRepository extends JpaRepository<Venta, Long> { }
